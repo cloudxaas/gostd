e1e68ae6480154e9b2e090f543cfcb5c7f304ab1
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-06 12:33:38 -0700

    Update httpheader.go

--- http/header/httpheader.go
+++ http/header/httpheader.go
@@ -1,6 +1,18 @@
 package httpheader
 
+
 const (
+  
+	GET     = 0
+	HEAD    = 1
+	POST    = 2
+	PUT     = 3
+	PATCH   = 4
+	DELETE  = 5
+	CONNECT = 6
+	OPTIONS = 7
+
+
   Gzip = "gz"
   Brotli = "br"
 )
